6af0ef57a6615814f6ba330e59b3510be6674461
grugrut <grugruglut+github@gmail.com>
2018-05-31 23:05:24 +0900

    :package: set up for heroku

--- lib/gultalis.ex
+++ lib/gultalis.ex
@@ -2,7 +2,7 @@ defmodule Gultalis do
   use Application
 
   def start(_type, _args) do
-    token = Application.get_env(:gultalis, :api_key)
+    token = System.get_env("API_KEY")
     {:ok, gultalis} = Slack.Bot.start_link(Gultalis.Slack, [], token)
   end
 end
