de9fd2c1fb884144b5b81ffe34e0191d9f12edea
grugrut <grugruglut+github@gmail.com>
2018-06-05 23:11:30 +0900

    :sparkles: add echo method

--- lib/gultalis/action.ex
+++ lib/gultalis/action.ex
@@ -1,8 +1,14 @@
 defmodule Gultalis.Action do
-  def hear("Hi", message, slack) do
+  def hear("Hi", _,  message, slack) do
+    IO.puts "Hi"
     Slack.Sends.send_message("Hello to you too!", message.channel, slack)
   end
 
-  def hear(_, _, _) do
+  def hear("echo", text, message, slack) do
+    IO.puts "echo"
+    Slack.Sends.send_message(text, message.channel, slack)
+  end
+
+  def hear(_, _, _, _) do
   end
 end
