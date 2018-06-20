f8590969bf8699e46ca9db1d40552df7a844e0f5
grugrut <grugruglut+github@gmail.com>
2018-06-20 22:38:48 +0900

    :recycle: refactoring

--- lib/gultalis/action.ex
+++ lib/gultalis/action.ex
@@ -1,12 +1,14 @@
 defmodule Gultalis.Action do
+  use Slack
+
   def hear("Hi", _, message, slack) do
     IO.puts("Hi")
-    Slack.Sends.send_message("Hello to you too!", message.channel, slack)
+    send_message("Hello to you too!", message.channel, slack)
   end
 
   def hear("echo", text, message, slack) do
     IO.puts("echo")
-    Slack.Sends.send_message(text, message.channel, slack)
+    send_message(text, message.channel, slack)
   end
 
   def hear("天気", _, message, slack) do
@@ -16,14 +18,14 @@ defmodule Gultalis.Action do
     body
     |> Floki.find("section.today-weather > div.weather-wrap.clearfix > div.weather-icon > p")
     |> Floki.text()
-    |> Slack.Sends.send_message(message.channel, slack)
+    |> send_message(message.channel, slack)
 
     body
     |> Floki.find("section.today-weather > div.weather-wrap.clearfix > div.date-value-wrap > dl")
     |> Floki.text()
-    |> Slack.Sends.send_message(message.channel, slack)
+    |> send_message(message.channel, slack)
 
-    Slack.Sends.send_message("https://tenki.jp/forecast/3/16/4410/13105/", message.channel, slack)
+    send_message("https://tenki.jp/forecast/3/16/4410/13105/", message.channel, slack)
   end
 
   def hear("ネタ", text, message, slack) do
@@ -40,7 +42,7 @@ defmodule Gultalis.Action do
         ]
       )
 
-    Slack.Sends.send_message(text <> "を登録しました", message.channel, slack)
+    send_message(text <> "を登録しました", message.channel, slack)
   end
 
   def hear(_, _, _, _) do
