cd19ec877d630a9f79063a1f6874165be2a9dd60
grugrut <grugruglut+github@gmail.com>
2018-12-30 15:46:06 +0900

    :sparkles: use supervisor

--- lib/gultalis.ex
+++ lib/gultalis.ex
@@ -2,7 +2,19 @@ defmodule Gultalis do
   use Application
 
   def start(_type, _args) do
-    token = System.get_env("API_KEY")
-    {:ok, gultalis} = Slack.Bot.start_link(Gultalis.Slack, [], token)
+    import Supervisor.Spec, warn: false
+
+    api_key =
+      case System.get_env("API_KEY") do
+        nil -> Application.get_env(:Gultalis, :api_key)
+        s -> s
+      end
+
+    children = [
+      worker(Slack.Bot, [Gultalis.Slack, [], api_key])
+    ]
+
+    opts = [strategy: :one_for_one, name: Gultalis.Supervisor]
+    {:ok, _pid} = Supervisor.start_link(children, opts)
   end
 end
